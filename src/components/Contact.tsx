b036a558d575cc08809501fdc743fabe9e85e0bc
harshavardhanaP96 <pkhrao3112.harsha@gmail.com>
2024-09-06 13:35:48 +0530

    contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -106,7 +106,7 @@ const ContactForm = () => {
       <div className="bg-white shadow-lg flex md:flex-row flex-col gap-10 rounded-lg p-8 w-full max-w-5xl mx-4 my-20">
         <div className="md:w-1/2 flex flex-col justify-center">
           <Image
-            src="https://plus.unsplash.com/premium_photo-1677529496297-fd0174d65941?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.pexels.com/photos/6930542/pexels-photo-6930542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="About Us"
             layout="responsive"
             height={100}
